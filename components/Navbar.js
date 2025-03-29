e31195124aa7c0b40e8f3004acd02222d4e45101
AV-PSU24 <abhinav11.velaga@gmail.com>
2025-03-29 17:55:44 -0400

    NavBar, Main Page layout completed

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,16 +2,102 @@ import styled from "styled-components";
 
 const NavStyle = styled.nav`
     display: flex;
-    position: fixed;
+    font-family: "Geist",sans-serif;
+    position: sticky;
+    justify-content: space-between;
     background-color: #0c0950;
     color: white;
     width: 100vw;
     height: 65px;
-
+    align-items: center;
 `;
 
-export default function NavBar(){
-    return(
-        <NavStyle> </NavStyle>
-    )
+const LeftDiv = styled.div`
+    display: flex;
+    align-items: center; 
+    justify-content: flex-start; 
+    padding-left: 20px; 
+`;
+
+const LogoDiv = styled.div`
+    display: flex;
+    justify-content: flex-start; 
+`;
+
+const Logo = styled.img`
+    width: 50px; 
+    height: 50px; 
+`;
+
+const TabsDiv = styled.div`
+    padding-left: 20px; 
+    display: flex;
+    align-items: center;
+    gap: 30px;
+`;
+
+const PageHeader = styled.a`
+    font-size: 16px;
+    color: white
+    cursor: pointer;
+    text-decoration: none;
+    transition: font-size 0.3s ease; 
+    &:hover {
+        font-size: 18px;
+    }
+    
+    &.active {
+        font-weight: bold;
+    }
+`;
+
+const RightDiv = styled.div`
+    display: flex;
+    padding-right: 20px; 
+`;
+
+const AccountDiv = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 30px;
+`;
+
+const SignUpBox = styled.div`
+    color: white; 
+    background-color: #006BFF;
+    padding: 10px 20px; 
+    border-radius: 7px; 
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); 
+    display: flex;
+    align-items: center; 
+    justify-content: center;
+
+    transition: background-color 0.3s ease;
+    &:hover {
+        background-color: #1D24CA;
+    }
+`;
+
+export default function NavBar() {
+    return (
+        <NavStyle>
+            <LeftDiv>
+                <LogoDiv>
+                    <Logo src="/images/bomboclaat_image.jpeg" alt="Logo" />
+                </LogoDiv>
+                <TabsDiv>
+                    <PageHeader>Search Groups</PageHeader>
+                    <PageHeader>My Events</PageHeader>
+                    <PageHeader>Study Buddies</PageHeader>
+                </TabsDiv>
+            </LeftDiv>
+            <RightDiv>
+                <AccountDiv>
+                    <PageHeader>Log In</PageHeader>
+                    <SignUpBox>Sign Up</SignUpBox>
+                </AccountDiv>
+            </RightDiv>
+        </NavStyle>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,69 @@
 import NavBar from "@/components/Navbar";
+import { styled } from "styled-components";
+
+const Body = styled.main`
+    flex-direction: column !important; 
+    display: flex;
+    font-family: "Geist",sans-serif; s 
+    align-items:flex-start;
+    padding-left: 20px;
+    padding-top: 30px;
+    gap: 300px;
+`;
+
+const ImgHContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    gap: 10px;
+`
+
+const HContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 20px;
+`;
+
+const Homeheading = styled.h1`
+    font-size: 36px;
+    font-weight: 300;
+    padding-bottom: 5px; 
+`;
+
+const Underline = styled.hr`
+    width: 75px; 
+    height: 3px; 
+    background-color: #40A2E3; 
+    border: none; 
+    margin: 5px 0; 
+`;
+
+const ImgIcon = styled.img`
+    width: 50px; 
+    height: 50px; 
+`;
 
 export default function Website(){
     return(
+        <> 
         <NavBar> </NavBar>
+        <Body> 
+            <ImgHContainer>
+                <ImgIcon src="/images/notbook.png" alt="Logo" />
+                <HContainer> 
+                    <Homeheading>Today's Events</Homeheading>
+                    <Underline/>
+                </HContainer>   
+            </ImgHContainer>
+            <ImgHContainer>
+                <ImgIcon src="/images/calender2.png" alt="Logo" />
+                <HContainer> 
+                    <Homeheading>Future Events</Homeheading>
+                    <Underline/>
+                </HContainer>   
+            </ImgHContainer>    
+        </Body>
+        </>
     )
 }
