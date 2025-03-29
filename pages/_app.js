e4edf3869d7b015b224c6fb07266170a471bb227
Krishna Panicker <krishnagopalpanicker@gmail.com>
2025-03-29 14:38:19 -0400

    Global Style created.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import "@/styles/globals.css";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0px;
+        padding: 0px;
+    }
+`;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+        <Head>
+            <title>PSU Group Study</title>
+            <meta name='description' content='Study group finder to help you meet fellow Penn State study buddies.'></meta>
+        </Head>
+        <GlobalStyle/>
+        <Component {...pageProps}/>
+    </> 
+  );
 }
