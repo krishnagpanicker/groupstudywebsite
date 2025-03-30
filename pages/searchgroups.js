e6d0897ee91ea277ac5de2a04c0acba0e4b9114c
AV-PSU24 <abhinav11.velaga@gmail.com>
2025-03-30 03:35:16 -0400

    Finished search options

--- pages/searchgroups.js
+++ pages/searchgroups.js
@@ -2,7 +2,8 @@ import NavBar from "@/components/Navbar";
 import { styled } from "styled-components";
 import { Heading,Underline } from "/";
 import { Body,FieldHeader,SelectorDiv,Spacer } from ".//myevents";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useStateContext } from "@/context/StateContext";
 
 const MySearchBody = styled.main`
     flex-direction: column !important; 
@@ -40,7 +41,9 @@ const HContainer = styled.div`
 const TextboxDiv = styled.div`
     flex-direction: column !important; 
     display: flex;
-    margin-left: 50px;
+    margin: 10px auto;
+    width: 80%;
+    
 `
 
 const InputTextbox = styled.input`
@@ -60,10 +63,41 @@ const Selector = styled.select`
 `
 
 export default function SearchGroupsPage(){
+    const { user } = useStateContext();
     const [isOpen, setIsOpen] = useState(false);
-    const [hour, setHour] = useState(12);
-    const [minute, setMinute] = useState(0);
-    const [AM, setAM] = useState("AM");
+    const [courseCode, setCourse] = useState("");
+    const [located, setLocation] = useState("");
+    const [described, setDescription] = useState("");
+    const [selectedHour1, setHour1] = useState(12);
+    const [selectedMinute1, setMinute1] = useState(0);
+    const [AM1, setAM1] = useState("AM");
+    const [selectedHour2, setHour2] = useState(12);
+    const [selectedMinute2, setMinute2] = useState(0);
+    const [AM2, setAM2] = useState("AM");
+
+    const [selectedMonth, setMonth] = useState(1);
+    const [selectedYear, setYear] = useState(2025);
+    const [selectedDay, setDay] = useState(1);
+    const [daysInMonth, setDaysInMonth] = useState([]);
+
+    useEffect(() => {
+        let numDays = 0;
+        let isLeapYear = selectedYear % 4 == 0;
+        if (selectedMonth == 1 || selectedMonth == 3 || selectedMonth == 5 || selectedMonth == 7 || selectedMonth == 8 || selectedMonth == 10 || selectedMonth == 12) {
+            numDays = 31;
+        }
+        else if (selectedMonth == 4 || selectedMonth == 6 || selectedMonth == 9 || selectedMonth == 11) {
+            numDays = 30;
+        }
+        else {
+            numDays = isLeapYear ? 29 : 28;
+        }
+        let days = [];
+        for (let i = 1; i <= numDays; i++) {
+            days.push(i);
+        }
+        setDaysInMonth(days);
+    }, [selectedMonth, selectedYear]);
 
     return(
         <>
@@ -82,7 +116,7 @@ export default function SearchGroupsPage(){
                     <InputTextbox type="text" placeholder="e.g. CMPEN 270"></InputTextbox>
                     <FieldHeader>Time</FieldHeader>
                             <SelectorDiv>
-                                <Selector id="hour" value={hour} onChange={(e) => setHour(e.target.value)}> 
+                                <Selector id="hour1" value={selectedHour1} onChange={(e) => setHour1(e.target.value)}> 
                                     <option value={12}>12</option>
                                     <option value={1}>01</option>
                                     <option value={2}>02</option>
@@ -95,19 +129,76 @@ export default function SearchGroupsPage(){
                                     <option value={9}>09</option>
                                     <option value={10}>10</option>
                                     <option value={11}>11</option>
-                                </Selector>
-                                <Spacer>:</Spacer>
-                                <Selector id="minute" value={minute} onChange={(e) => setMinute(e.target.value)}> 
+                                </Selector>                                
+                                <Selector id="minute1" value={selectedMinute1} onChange={(e) => setMinute1(e.target.value)}> 
                                     <option value={0}>00</option>
                                     <option value={15}>15</option>
                                     <option value={30}>30</option>
                                     <option value={45}>45</option>
                                 </Selector>
-                                <Selector id="AM_PM" value={AM} onChange={(e) => setAM(e.target.value)}> 
+                                <Selector id="AM_PM1" value={AM1} onChange={(e) => setAM1(e.target.value)}> 
+                                    <option value={"AM"}>AM</option>
+                                    <option value={"PM"}>PM</option>
+                                </Selector>
+                                <Spacer>to</Spacer>
+                                <Selector id="hour2" value={selectedHour2} onChange={(e) => setHour2(e.target.value)}> 
+                                    <option value={12}>12</option>
+                                    <option value={1}>01</option>
+                                    <option value={2}>02</option>
+                                    <option value={3}>03</option>
+                                    <option value={4}>04</option>
+                                    <option value={5}>05</option>
+                                    <option value={6}>06</option>
+                                    <option value={7}>07</option>
+                                    <option value={8}>08</option>
+                                    <option value={9}>09</option>
+                                    <option value={10}>10</option>
+                                    <option value={11}>11</option>
+                                </Selector>                                
+                                <Selector id="minute2" value={selectedMinute2} onChange={(e) => setMinute2(e.target.value)}> 
+                                    <option value={0}>00</option>
+                                    <option value={15}>15</option>
+                                    <option value={30}>30</option>
+                                    <option value={45}>45</option>
+                                </Selector>
+                                <Selector id="AM_PM2" value={AM2} onChange={(e) => setAM2(e.target.value)}> 
                                     <option value={"AM"}>AM</option>
                                     <option value={"PM"}>PM</option>
                                 </Selector>
                             </SelectorDiv>
+                            <FieldHeader>Date</FieldHeader>
+                            <SelectorDiv>
+                                <Selector id="month" value={selectedMonth} onChange={(e) => setMonth(e.target.value)}>
+                                    <option value={1}>January</option>
+                                    <option value={2}>February</option>
+                                    <option value={3}>March</option>
+                                    <option value={4}>April</option>
+                                    <option value={5}>May</option>
+                                    <option value={6}>June</option>
+                                    <option value={7}>July</option>
+                                    <option value={8}>August</option>
+                                    <option value={9}>September</option>
+                                    <option value={10}>October</option>
+                                    <option value={11}>November</option>
+                                    <option value={12}>December</option>
+                                </Selector>
+                                <Spacer>/</Spacer>
+                                <Selector id="day" value={selectedDay} onChange={(e) => setDay(e.target.value)}>
+                                    {daysInMonth.map((day) => (
+                                        <option key={day} value={day}>{day}</option>
+                                    ))}
+                                </Selector>
+                                <Spacer>/</Spacer>
+                                <Selector id="year" value={selectedYear} onChange={(e) => setYear(e.target.value)}>
+                                    <option value={2025}>2025</option>
+                                    <option value={2026}>2026</option>
+                                    <option value={2027}>2027</option>
+                                    <option value={2028}>2028</option>
+                                    <option value={2029}>2029</option>
+                                </Selector>
+                            </SelectorDiv>
+                            <FieldHeader>Location</FieldHeader>
+                            <InputTextbox type="text" placeholder="e.g. Haller 107" onChange={(e) => setLocation(e.target.value)}></InputTextbox>
                 </TextboxDiv>
             </MySearchBody>
             
